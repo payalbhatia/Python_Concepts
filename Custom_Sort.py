e1193c5eb2a6ff4cac13e43ed7e1086adc7cbc2f
payalbhatia <33825481+payalbhatia@users.noreply.github.com>
2021-08-11 18:21:55 +0530

    Update Custom_Sort.py

--- Custom_Sort.py
+++ Custom_Sort.py
@@ -8,3 +8,10 @@ word_list = ['Python', 'is', 'better', 'than', 'C']
 word_list.sort(key=compare_num_of_chars)
 print(word_list)
 #['C', 'is', 'than', 'Python', 'better']
+
+#sort the list by second element of each list 
+lists1=[[1, 2, 3], [2, 1, 3], [4, 0, 1]]
+lists2=sorted(lists1, key=lambda x: x[1])
+print (lists2)
+
+#[[4, 0, 1], [2, 1, 3], [1, 2, 3]]
